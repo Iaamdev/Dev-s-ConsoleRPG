bd7631af20363e7b23b14419e4dd66e89906e01e
Iaamdev <139625151+Iaamdev@users.noreply.github.com>
2025-11-21 13:12:23 -0800

    (May not need this but pushing anyway!)

---
+++ src/Fighter.cpp
@@ -0,0 +1,7 @@
+#include "../include/Fighter.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Player::Fighter(string name, int health, int attackPower, double startingDefenseBonus = 0.15) {}
