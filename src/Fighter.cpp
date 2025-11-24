61ef11ec672de39d4d14a9e3d3f666dcb645403e
Iaamdev <139625151+Iaamdev@users.noreply.github.com>
2025-11-24 10:39:16 -0800

    Deleted Fighter.cpp file

--- src/Fighter.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/Fighter.h"
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-Player::Fighter(string name, int health, int attackPower, double startingDefenseBonus = 0.15) {}
