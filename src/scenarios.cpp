52613b56d2832d14a277ae32ef60b256926bf9c5
Iaamdev <139625151+Iaamdev@users.noreply.github.com>
2025-12-12 10:50:44 -0800

    Small changed

--- src/scenarios.cpp
+++ src/scenarios.cpp
@@ -1,6 +1,7 @@
 #include "../include/scenarios.h"
 #include "../include/Enemy.h"
 #include "../include/Battle.h"
+#include "../include/ascii.h"
 #include <iostream>
 #include <thread> 
 #include <chrono>
@@ -10,13 +11,16 @@
 
 using namespace std;
 
+//NOTE: I want to add medium difficulty scenarios and make the Hard one more difficult
+//(maybe nerf player attack damage)
+
 void wait() {
   std::this_thread::sleep_for(std::chrono::seconds(2));
 }
 
 void explore(Player& hero) {
   cout << "\n----------------------------------------" << endl;
-  cout << "You stand at a fork in the road..." << endl;
+  cout << "You stand at a fork in the road...\n" << endl;
     
   vector<string> safePaths = {
     "A well-lit path through a sunny meadow.",
@@ -49,10 +53,10 @@ void explore(Player& hero) {
   int scenarioID = 0;
 
   if (choice == 1) {
-    cout << "\nYou choose the safer route..." << endl;
+    cout << "\nYou choose the safer route...\n" << endl;
     scenarioID = (rand() % 5) + 1; 
   } else {
-    cout << "\nYou steel your nerves and brave the dark path..." << endl;
+    cout << "\nYou steel your nerves and brave the dark path...\n" << endl;
     scenarioID = (rand() % 5) + 6;
   }
 
@@ -68,87 +72,98 @@ void runScenario(Player& hero, int scenarioID) {
   switch (scenarioID) {
     case 1: {
       cout << "You start your journey in the local tavern basement." << endl;
-      cout << "The innkeeper asked you to clear out some pests..." << endl;
+      cout << "The innkeeper asked you to clear out some pests...\n" << endl;
       wait();
+      ratAscii();
       Enemy rat("Giant Rat", 30, 3, 20); 
       Battle(hero, rat);
       break;
     }
     case 2: {
       cout << "Leaving the town, you travel through the Whispering Woods." << endl;
-      cout << "Suddenly, a green figure jumps from the bushes!" << endl;
+      cout << "Suddenly, a green figure jumps from the bushes!\n" << endl;
       wait();
+      goblinAscii();
       Enemy goblin("Goblin Scout", 50, 6, 40);
       Battle(hero, goblin);
       break;
     }
     case 3: {
       cout << "Night falls upon your camp. You hear howling nearby." << endl;
-      cout << "A pair of glowing yellow eyes stares at you from the darkness." << endl;
+      cout << "A pair of glowing yellow eyes stares at you from the darkness.\n" << endl;
       wait();
+      direWolfAscii();
       Enemy wolf("Dire Wolf", 70, 9, 60);
       Battle(hero, wolf);
       break;
     }
     case 4: {
       cout << "You reach the Old Stone Bridge." << endl;
-      cout << "A ruffian blocks your path. 'Your money or your life!' he shouts." << endl;
+      cout << "A ruffian blocks your path. 'Your money or your life!' he shouts.\n" << endl;
       wait();
+      banditAscii();
       Enemy bandit("Roadside Bandit", 90, 11, 80); 
       Battle(hero, bandit);
       break;
     }
     case 5: {
       cout << "Seeking treasure, you enter the Ancient Crypts." << endl;
-      cout << "The bones in the corner begin to rattle and assemble..." << endl;
+      cout << "The bones in the corner begin to rattle and assemble...\n" << endl;
       wait();
+      skeletonAscii();
       Enemy skeleton("Skeleton Warrior", 110, 13, 100);
       Battle(hero, skeleton);
       break;
     }
+    //TODO: Buff the Hard scenarios significantly making them much more challenging
     case 6: {
       cout << "You find an abandoned outpost occupied by a warband." << endl;
-      cout << "A massive greenskin brute charges at you!" << endl;
+      cout << "A massive greenskin brute charges at you!\n" << endl;
       wait();
-      Enemy orc("Orc Berserker", 140, 16, 130);
+      orcAscii();
+      Enemy orc("Orc Berserker", 200, 26, 130);
       Battle(hero, orc);
       break;
     }
     case 7: {
       cout << "You enter the swamp. The smell is terrible." << endl;
-      cout << "A large, regenerating creature emerges from the muck." << endl;
+      cout << "A large, regenerating creature emerges from the muck.\n" << endl;
       wait();
-      Enemy troll("Swamp Troll", 180, 12, 160); 
+      trollAscii();
+      Enemy troll("Swamp Troll", 250, 28, 160); 
       Battle(hero, troll);
       break;
     }
     case 8: {
       cout << "You approach the Dark Tower." << endl;
-      cout << "A hooded figure chants an incantation, summoning dark energy." << endl;
+      cout << "A hooded figure chants an incantation, summoning dark energy.\n" << endl;
       wait();
-      Enemy cultist("Dark Sorcerer", 120, 25, 200); 
+      sorcererAscii(); 
+      Enemy cultist("Dark Sorcerer", 300, 30, 200); 
       Battle(hero, cultist);
       break;
     }
     case 9: {
       cout << "Guarding the throne room is the King's fallen champion." << endl;
-      cout << "His armor is black as night, and he speaks no words." << endl;
+      cout << "His armor is black as night, and he speaks no words.\n" << endl;
       wait();
-      Enemy knight("Death Knight", 250, 22, 350); 
+      deathKnightAscii();
+      Enemy knight("Death Knight", 350, 32, 350); 
       Battle(hero, knight);
       break;
     }
     case 10: {
       // cout << "THE FINAL BATTLE." << endl;
       cout << "You stand before the ancient evil that plagues the land." << endl;
-      cout << "The Dragon roars, shaking the very foundations of the earth!" << endl;
+      cout << "The Dragon roars, shaking the very foundations of the earth!\n" << endl;
       wait();
+      dragonAscii();
       Enemy dragon("Red Dragon", 500, 40, 1000); 
       Battle(hero, dragon);
       break;
     }
     default:
-      cout << "The path is quiet. Nothing happens." << endl;
+      cout << "The path is quiet. Nothing happens.\n" << endl;
       break;
     }
 }
